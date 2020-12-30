d779327300df84d3743838688622396497f345cc
Charlie Meers <charlie.meers@lifestylepubs.com>
2020-12-29 22:34:55 -0800

    Require jQuery

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-import './css/main.scss';
+import $ from 'jquery'
+window.jQuery = $;
+window.$ = $;
+
+import './common.blocks/main.scss';
+import './common.blocks/slider.js';
+
+$(() => {
+    $('a').click(() => {return false;});
+});
